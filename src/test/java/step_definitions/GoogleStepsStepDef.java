27207f7e9f9071eac08b5ba7249371677fae4fd1
Nargiza Umali <Nakitay84@gmail.com>
2026-04-14 17:28:46 -0600

    Fixed apple title assertion

--- src/test/java/step_definitions/GoogleStepsStepDef.java
+++ src/test/java/step_definitions/GoogleStepsStepDef.java
@@ -40,7 +40,8 @@ public class GoogleStepsStepDef {
 
     @Then("user sees apple in the page title")
     public void userSeesAppleInThePageTitle() {
-        Assert.assertTrue(Driver.getDriver().getTitle().contains("apple"));
+
+        Assert.assertTrue(Driver.getDriver().getTitle().toLowerCase().contains("apple"));
     }
 
     @When("user types orange in the google search box and click enter")
